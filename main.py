520056e2f0a831caa3074ec92f7336eab6adaf5e
Mukadas <mukadasadylbekova@gmail.com>
2025-03-19 13:30:44 +0600

    update the files

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from window import CalculatorWindow
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = CalculatorWindow()
+    window.show()
+    sys.exit(app.exec_())
+    
